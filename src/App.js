31c426e6462ec9f76695216a9f6fdb22b9d1982a
vainikkk <vainik.kakadia@gmail.com>
2025-05-16 08:08:18 -0400

    web pack remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import JobCards from './pages/JobCardDetails';
+// import JobCards from './pages/JobCardDetails';
 // import Customers from './pages/Customers';
 // import Vehicles from './pages/Vehicles';
 import Services from './pages/Services';
